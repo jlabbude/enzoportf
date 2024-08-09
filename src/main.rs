5b67c9f7497bd0cd0343c6496a834912b528c278
Lucas <jlabbude@hotmail.com>
2024-08-08 21:57:28 -0300

    reworked get_image endpoint

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,52 @@ mod models;
 mod schema;
 
 use db::Db;
-use diesel::prelude::*;
+use diesel::{dsl::insert_into, prelude::*};
 use models::Image;
 
-#[get("/images/<id>")]
-async fn get_image(db: Db, id: i32) -> Option<Vec<u8>> {
+use schema::images::dsl::*;
+
+use rocket::http::{ContentType, Status};
+use rocket::response::{Builder, status};
+use rocket::serde::json::Json;
+use rand::random;
+use rocket::Response;
+
+#[get("/images?<id>")]
+async fn get_image(db: Db, id: i32) -> Result<(ContentType, Vec<u8>), String> {
     db.run(move |conn| {
-        use schema::images::dsl::*;
         images
             .filter(img_id.eq(id))
             .first::<Image>(conn)
             .ok()
-            .map(|img: Image| std::fs::read(img.img_path).unwrap())
+            .and_then(|img: Image| std::fs::read(img.img_path).ok())
+            .ok_or("Image not found".to_string())
+            .map(|img| (ContentType::PNG, img))
+    })
+        .await
+}
+
+#[post("/postimage?<path>")]
+async fn post_image(db: Db, path: String) -> Result<Json<usize>, status::Custom<String>> {
+    db.run(move |conn| {
+        insert_into(images)
+            .values(Image {
+                img_id: random::<i32>(), // todo uid gen
+                img_path: path,
+            })
+            .execute(conn)
     })
     .await
+    .map(Json)
+    .map_err(|err| {
+        let error_message = format!("Failed to insert image: {:?}", err);
+        status::Custom(Status::InternalServerError, error_message)
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(Db::fairing())
-        .mount("/", routes![get_image])
+        .mount("/", routes![get_image, post_image])
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
-#[derive(diesel::Queryable)]
+#[derive(diesel::Queryable, diesel::Insertable)]
+#[diesel(table_name = crate::schema::images)]
 pub struct Image {
     pub img_id: i32,
     pub img_path: String,
