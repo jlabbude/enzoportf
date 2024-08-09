d9e4382037ed9e71361de11cf1121b228d5dbeb1
Lucas <jlabbude@hotmail.com>
2024-08-09 00:08:38 -0300

    wip

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,14 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+           <img src="http://localhost:8000/images?id=1539493375"/>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
