3e8053300be83e37af419cc2e6adcaa7f2640fca
Lucas <jlabbude@hotmail.com>
2024-08-11 00:06:53 -0300

    test implementation of tailwind

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,12 +4,13 @@ use yew::prelude::*;
 
 #[hook]
 fn use_fetch_image_vec() -> UseStateHandle<Vec<Value>> {
-    #[allow(clippy::redundant_closure)] // Idk why Clippy keeps bugging about this, when the function can only take closures as args
+    #[allow(clippy::redundant_closure)]
+    // Idk why Clippy keeps bugging about this, when the function can only take closures as args
     let images = use_state_eq(|| Vec::<Value>::new());
     {
         let images = images.clone();
         spawn_local(async move {
-            let fetched_images = reqwest::get("http://127.0.0.1:8000/all-images")
+            let fetched_images = reqwest::get("http://localhost:8000/all-images")
                 .await
                 .unwrap()
                 .json::<Vec<Value>>()
@@ -26,19 +27,20 @@ fn App() -> Html {
     let images = use_fetch_image_vec();
 
     html! {
-        <div id="images">
-            {
-                (*images).iter().map(|image_array| {
-                    html! {
-                        <img src={format!("http://127.0.0.1:8000/images?id={}",
-                                  image_array["img_id"],
-                             )}
-                             width="128"
-                             height="128" />
-                    }
-                }).collect::<Html>()
-            }
-        </div>
+        <>
+            <header> <h1 style="text-align: center; font-size: 50px;">{"TODO"}</h1></header>
+            <div id="imagaes">
+                {
+                    (*images).iter().map(|image_array| {
+                        html! {
+                            <img src={format!("/api/images?id={}",
+                                    image_array["img_id"],
+                                )}class="rounded-full"/>
+                        }
+                    }).collect::<Html>()
+                }
+            </div>
+        </>
     }
 }
 
