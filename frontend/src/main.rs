5879d1ae438566110136ca5650cb1fae9a0a9a99
Lucas <jlabbude@hotmail.com>
2024-08-13 00:35:33 -0300

    make image fetching extensible

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,33 +1,55 @@
 use serde_json::Value;
-use wasm_bindgen_futures::spawn_local;
 use yew::prelude::*;
 
-struct ReturnedImage {
+#[derive(PartialEq)]
+pub struct ReturnedImage {
     img_id: u8,
     img_path: String,
 }
 
 pub trait RequestedImage {
-    fn handle_images(images: UseStateHandle<Vec<Value>>) -> UseStateHandle<Vec<Value>> {
-        let images = images.clone();
-        spawn_local(async move {
-            reqwest::get("http://localhost:8000/all-images")
-                .await
-                .unwrap()
-                .json::<Vec<Value>>()
-                .await
-                .unwrap();
-        });
+    fn fetch_images(
+        images: UseStateHandle<Vec<ReturnedImage>>,
+    ) -> UseStateHandle<Vec<ReturnedImage>>;
+
+    fn serialize_json(val: &Value) -> ReturnedImage;
+}
+
+impl RequestedImage for ReturnedImage {
+    fn serialize_json(val: &Value) -> ReturnedImage {
+        ReturnedImage {
+            img_id: val["img_id"].as_u64().unwrap() as u8,
+            img_path: val["img_path"].to_string(),
+        }
+    }
+
+    fn fetch_images(
+        images: UseStateHandle<Vec<ReturnedImage>>,
+    ) -> UseStateHandle<Vec<ReturnedImage>> {
+        {
+            let images = images.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                images.set(
+                    reqwest::get("http://localhost:8000/all-images")
+                        .await
+                        .unwrap()
+                        .json::<Vec<Value>>()
+                        .await
+                        .unwrap()
+                        .iter()
+                        .map(ReturnedImage::serialize_json)
+                        .collect::<Vec<ReturnedImage>>(),
+                );
+            });
+        }
         images
     }
 }
 
-impl RequestedImage for ReturnedImage {}
-
 #[hook]
-fn use_fetch_image_vec() -> UseStateHandle<Vec<Value>> {
+fn use_fetch_image_vec() -> UseStateHandle<Vec<ReturnedImage>> {
     #[allow(clippy::redundant_closure)]
-    <ReturnedImage as RequestedImage>::handle_images(use_state_eq(|| Vec::<Value>::new()))
+    <ReturnedImage as RequestedImage>::fetch_images(use_state_eq(|| Vec::<ReturnedImage>::new()))
 }
 
 #[function_component]
@@ -42,7 +64,7 @@ fn App() -> Html {
                     (*images).iter().map(|image_array| {
                         html! {
                             <img src={format!("/api/images?id={}",
-                                    image_array["img_id"],
+                                    image_array.img_id,
                                 )} class="rounded-s-full "/>
                         }
                     }).collect::<Html>()
